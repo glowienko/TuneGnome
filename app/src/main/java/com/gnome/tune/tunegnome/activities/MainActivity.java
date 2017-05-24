9ca679829e0ed221a6f32d91f7da872020a4e148
Patryk <glowienko.patryk@gmail.com>
2017-05-24 20:50:15 +0200

    added proper sound measurement and volume adjusting algorithm

--- app/src/main/java/com/gnome/tune/tunegnome/activities/MainActivity.java
+++ app/src/main/java/com/gnome/tune/tunegnome/activities/MainActivity.java
@@ -1,40 +1,51 @@
 package com.gnome.tune.tunegnome.activities;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.TextView;
 
 import com.gnome.tune.tunegnome.R;
 import com.gnome.tune.tunegnome.actions.TuneGnomeActions;
 import com.gnome.tune.tunegnome.services.NoiseMeasuringService;
+import com.gnome.tune.tunegnome.storage.ProfilesDatabaseHelper;
 import com.gnome.tune.tunegnome.utils.NoiseLevelNotification;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView callSoundLevel;
+    private AudioManager audioManager;
+    private ProfilesDatabaseHelper dbHelper;
 
+
+    int maxPhoneVolume;
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     private boolean permissionToRecordAccepted = false;
     private String permissionName = Manifest.permission.RECORD_AUDIO;
     private String[] permissions = {Manifest.permission.RECORD_AUDIO};
+    private boolean isProfileActive;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_page);
-        permissionToRecordAccepted = ContextCompat.checkSelfPermission(this, permissionName) == PackageManager.PERMISSION_GRANTED;
-        callSoundLevel = (TextView) findViewById(R.id.callSoundLevel);
+        isProfileActive = false;
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        maxPhoneVolume = audioManager.getStreamMaxVolume(audioManager.STREAM_RING);
+        dbHelper = new ProfilesDatabaseHelper(this);
 
+        permissionToRecordAccepted = ContextCompat.checkSelfPermission(this, permissionName) == PackageManager.PERMISSION_GRANTED;
     }
 
     public void startMeasureNoise(View view) {
-        if (permissionToRecordAccepted) {
+//        checkIfProfileActive();
+
+        if (permissionToRecordAccepted && !isProfileActive) {
             startMeasureService();
         } else {
             ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
@@ -72,5 +83,25 @@ public class MainActivity extends AppCompatActivity {
 
     public void startUserProfileActivity(View view) {
         startActivity(new Intent(getApplicationContext(), UserProfileActivity.class));
+    }
+
+
+    //Metoda, która ma za zadanie sprawdzenie, czy aktualny czas jest uwzględniony w profilowaniu
+    public void checkIfProfileActive() {
+//        Calendar cal = Calendar.getInstance();
+//        cal.setTime(new Date());
+//        Double hours = (double) cal.get(Calendar.HOUR_OF_DAY);
+//
+//
+//
+//        //  float currTime = (float)new Date().getTime();
+//        if ((isBetween((hours), beginSleepTime, endSleepTime)))
+//            audioManager.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+//        else
+//            audioManager.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+    }
+
+    public static boolean isBetween(Double x, int floorValue, int cellValue) {
+        return floorValue <= x && x <= cellValue;
     }
 }
--- app/src/main/java/com/gnome/tune/tunegnome/services/NoiseMeasuringService.java
+++ app/src/main/java/com/gnome/tune/tunegnome/services/NoiseMeasuringService.java
@@ -1,7 +1,9 @@
 package com.gnome.tune.tunegnome.services;
 
 import android.app.IntentService;
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaRecorder;
 import android.util.Log;
 
@@ -9,10 +11,11 @@ import com.gnome.tune.tunegnome.actions.TuneGnomeActions;
 import com.gnome.tune.tunegnome.utils.NoiseLevelNotification;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import static com.gnome.tune.tunegnome.actions.TuneGnomeActions.NOISE_PARAM;
 
 /**
  * An {@link IntentService} subclass for handling asynchronous task requests in
@@ -22,8 +25,10 @@ public class NoiseMeasuringService extends IntentService {
 
     private static final String LOG_TAG = "NOISE_SERVICE: ";
     private MediaRecorder recorder;
-    private short[] buffer;
     private Timer timer;
+    //Lista, w której będzie przechowywanych 10 ostatnich pomiarów głośności
+    ArrayList<Double> noiseLevels;
+    private AudioManager audioManager;
 
     public NoiseMeasuringService() {
         super("NoiseMeasuringService");
@@ -42,16 +47,18 @@ public class NoiseMeasuringService extends IntentService {
     }
 
     private void handleActionMeasureNoise() {
+        noiseLevels = new ArrayList<>();
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
         setupMediaRecorder();
         startMeasurement();
     }
 
     private void startMeasurement() {
-
         try {
             recorder.prepare();
             recorder.start();
-        } catch(IllegalStateException | IOException e) {
+        } catch (IllegalStateException | IOException e) {
             e.printStackTrace();
         }
 
@@ -62,32 +69,19 @@ public class NoiseMeasuringService extends IntentService {
     private TimerTask createTimerTaskForMeasurement() {
         return new TimerTask() {
             public void run() {
-
-                double currentNoiseLevel = 0;
-                double maxAmplitude = 0;
-
-
-                if (maxAmplitude != 0) {
-                    currentNoiseLevel = 20.0 * Math.log10(maxAmplitude / 32767.0) + 90;
-                }
+                double currentNoiseLevel = 20 * Math.log10(Math.abs(recorder.getMaxAmplitude()));
+//               double currentNoiseLevel = 60;
                 Log.d(LOG_TAG, Double.toString(currentNoiseLevel));
 
-                broadcastNoiseValue(currentNoiseLevel);
-                NoiseLevelNotification.createOrUpdate(getApplicationContext(), Double.toString(currentNoiseLevel));
+                if(noiseLevels.size() == 10) {
+                    proceedingAlgorithm(calculateAverageNoiseLevel(noiseLevels));
+                    NoiseLevelNotification.createOrUpdate(getApplicationContext(), Double.toString(currentNoiseLevel));
+                    noiseLevels.clear();
+                }
+                noiseLevels.add(currentNoiseLevel);
             }
         };
     }
-
-    private void broadcastNoiseValue(double noiseValue) {
-        Intent intent = new Intent(getApplicationContext(), VolumeAdjusterService.class);
-        intent.setAction(TuneGnomeActions.ACTION_NOISE_VALUE);
-        intent.putExtra(NOISE_PARAM, noiseValue);
-
-        getApplicationContext().startService(intent);
-        Log.d(LOG_TAG, "noise passed to another service");
-    }
-
-
     @Override
     public void onDestroy() {
         super.onDestroy();
@@ -96,6 +90,7 @@ public class NoiseMeasuringService extends IntentService {
         if (recorder != null) {
             recorder.stop();
             recorder.release();
+            recorder.reset();
             recorder = null;
             if (timer != null) {
                 timer.cancel();
@@ -107,11 +102,78 @@ public class NoiseMeasuringService extends IntentService {
     }
 
     private void setupMediaRecorder() {
-        MediaRecorder recorder = new MediaRecorder();
+
+        recorder = new MediaRecorder();
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
         recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
         recorder.setOutputFile("/dev/null");
+    }
+
+    //Metoda, która zwraca wyliczony średni poziom głośności
+    //Algorytm działania:
+    //1. Usuwa dwie najmniejsze i dwie największe wartości z listy ostatnich pomiarów
+    //2. Wylicza średnią arytmetyczną z pozostałych wartości
+    protected Double calculateAverageNoiseLevel(ArrayList<Double> NoiseArray) {
+        Double sum = 0.0;
+        List<Double> tmpNoiseArray = NoiseArray;
+        tmpNoiseArray.remove(tmpNoiseArray.indexOf(Collections.min(tmpNoiseArray)));
+        tmpNoiseArray.remove(tmpNoiseArray.indexOf(Collections.min(tmpNoiseArray)));
+        tmpNoiseArray.remove(tmpNoiseArray.indexOf(Collections.max(tmpNoiseArray)));
+        tmpNoiseArray.remove(tmpNoiseArray.indexOf(Collections.max(tmpNoiseArray)));
+        for (int i = 0; i < tmpNoiseArray.size(); i++) {
+            Double currentNumber = tmpNoiseArray.get(i).doubleValue();
+            sum += currentNumber;
+        }
+        return sum / tmpNoiseArray.size();
+    }
+
+
+    //Metoda przetwarzająca średni pomiar algorytmu
+    protected boolean proceedingAlgorithm(Double averagedNoiseLevel) {
+        if (isBetween(averagedNoiseLevel, 1, 35))
+        {
+            audioManager.setStreamVolume(audioManager.STREAM_RING, 1, 0);
+            return true;
+        }
+        else if (isBetween(averagedNoiseLevel, 35, 43))
+        {
+            audioManager.setStreamVolume(audioManager.STREAM_RING, 1, 0);
+            return true;
+        }
+        else if (isBetween(averagedNoiseLevel, 43, 47))
+        {
+            audioManager.setStreamVolume(audioManager.STREAM_RING, 1, 0);
+            return true;
+        }
+        else if (isBetween(averagedNoiseLevel, 43, 47))
+        {
+            audioManager.setStreamVolume(audioManager.STREAM_RING, 1, 0);
+            return true;
+        }
+        else if (isBetween(averagedNoiseLevel, 43, 47))
+        {
+            audioManager.setStreamVolume(audioManager.STREAM_RING, 1, 0);
+            return true;
+        }
+        else if (isBetween(averagedNoiseLevel, 47, 48))
+        {
+            audioManager.setStreamVolume(audioManager.STREAM_RING, 1, 0);
+            return true;
+        }
+        else if (isBetween(averagedNoiseLevel, 83, 90))
+        {
+            audioManager.setStreamVolume(audioManager.STREAM_RING, 1, 0);
+            return true;
+        }
+        else
+            return false;
+
+
+    }
+
+    public static boolean isBetween(Double x, int floorValue, int cellValue) {
+        return floorValue <= x && x <= cellValue;
     }
 
 }
